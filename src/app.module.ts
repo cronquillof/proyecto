699a497df72d4221e3066e653f1b31c5c6c5145c
cronquillof <88749701+cronquillof@users.noreply.github.com>
2024-01-10 19:00:49 -0500

    master

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModuleModule } from './module/module.module';
+import { CommonModule } from './common/common.module';
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [],
+  imports: [ModuleModule, CommonModule, UtilsModule],
   controllers: [AppController],
   providers: [AppService],
 })
